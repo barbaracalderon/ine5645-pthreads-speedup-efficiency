26535407559f179e63154e6394afad8314a07eb3
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-11-22 23:35:19 -0300

    Created the t_function which the pthread will use

--- reproduz_texto_paralelo2.c
+++ reproduz_texto_paralelo2.c
@@ -17,3 +17,44 @@ int cria_palavra_secreta (char *palavra, int tam) {
     palavra[i] = 32 + rand() % 94;
     palavra[tam-1] = '\0';
 };
+
+void *t_function (void *arg) {
+    int j, k;
+    long *rank;
+    rank = (long *)arg;
+
+    printf("-- Thread %ld começou", *rank);
+    printf("\n");
+
+        // Última thread
+        if (*rank == (NUM_THREADS-1)) {
+            printf("Última thread...\n");
+            for (j = (NUM_THREADS-1)*intervalo; j < tamanho; j++) {
+                for (k = 0; k < 255; k++) {
+                    chute[j] = k;
+                    if (chute[j] == texto[j]) {
+                        j=0;
+                        break;
+                    };
+                };
+                        break;
+            };
+        }
+        // Outras threads
+        else {
+            printf("Thread...\n");
+            for (j = (*rank)*intervalo; j < ((*rank)+1)*intervalo; j++) {
+                for (k = 0; k < 255; k++) {
+                    chute[j] = k;
+                    if (chute[j] == texto[j]) {
+                        j=0;
+                        break;
+                    };
+                };
+                        break;
+            };
+        };
+    
+    printf("-- Thread %ld terminou\n", *rank);
+    pthread_exit(NULL);
+}
