e8afdbc91d8a57a26ca705d86fa0273b630da9da
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-11-22 23:34:09 -0300

    Created cria_palavra_secreta function and defined some variables

--- reproduz_texto_paralelo2.c
+++ reproduz_texto_paralelo2.c
@@ -4,3 +4,16 @@
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
+#define NUM_THREADS 32
+
+char *texto;
+char *chute;
+int tamanho;
+int intervalo;
+
+int cria_palavra_secreta (char *palavra, int tam) {
+    srand((unsigned) time(NULL));
+    for (int i = 0; i < tam-1; i++)
+    palavra[i] = 32 + rand() % 94;
+    palavra[tam-1] = '\0';
+};
